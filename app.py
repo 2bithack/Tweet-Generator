80e386ec6f53f8f1476f2c80229b37d4179f1869
enzobotter <enzobot@hotmail.com>
2017-02-13 14:21:07 -0800

    it works!

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! Your random word from the great poem "Mary had a little lamb" is:' + str(stochastic2.random_word())
+    return 'Hello, World! Your random word from the great poem "Mary had a little lamb" is: ' + str(stochastic2.random_word())
